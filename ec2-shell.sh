52aa929c955223a86a6138dd2038516c21e19a20
sukumar <sukumarg.7310@gmail.com>
2025-07-10 12:34:30 +0530

    instance creation

--- ec2-shell.sh
+++ ec2-shell.sh
@@ -2,7 +2,7 @@
 
 AMI=ami-05ffe3c48a9991133
 SG_ID=sg-050ef4889bc4a5a82
-INSTANCES=("mongodb" "user")
+INSTANCES=("mongodb" "catalogue")
 
 for i in "${INSTANCES[@]}"
 do
@@ -13,7 +13,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-05ffe3c48a9991133 --instance-type $INSTANCE_TYPE  --security-group-ids sg-050ef4889bc4a5a82)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-05ffe3c48a9991133 --instance-type $INSTANCE_TYPE  --security-group-ids sg-050ef4889bc4a5a82 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'INSTANCE[0].PrivateIpAddress' --output text)
    
     echo "$i: $IP_ADDRESS"
 done
