070fbdaf0cf42c70526ea2cf23a7aa2cabb497f3
sukumar <sukumarg.7310@gmail.com>
2025-07-10 15:10:53 +0530

    DISK USAGE ALERT

---
+++ disk-usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+MESSAGE=""
+
+while IFS= read -r line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+="High Disk Usage on $partition: $usage\n"
+
+done <<< $DISK_USAGE
+
+echo "message: $MESSAGE"
