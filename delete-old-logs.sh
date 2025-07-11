089ee5df483743a2da9ea55ebb6d08e33142e4b5
sukumar <sukumarg.7310@gmail.com>
2025-07-11 08:46:03 +0530

    delete old logs

---
+++ delete-old-logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shell-logs"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d SOURCE_DIR ]
+then
+    echo -e "$R Source directory: $SOURCE_DIR does not exist. $N"
+fi 
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +31 -name "*.log")
+
+while IFS= read -r line
+do 
+    echo "Deleting file: $line"
+done <<< $FILES_TO_DELETE
