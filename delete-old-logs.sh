11d8e1107afef0ffdb26ea03c52535a12992c32f
sukumar <sukumarg.7310@gmail.com>
2025-07-11 09:32:33 +0530

    delete old logs

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -10,9 +10,10 @@ N="\e[0m"
 if [ ! -d SOURCE_DIR ]
 then
     echo -e "$R Source directory: $SOURCE_DIR does not exist. $N"
+    exit 1
 fi 
 
-FILES_TO_DELETE=$(find . -type f -mtime +31 -name "*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +31 -name "*.log")
 
 while IFS= read -r line
 do 
