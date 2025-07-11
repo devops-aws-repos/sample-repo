69401785e674f53ab23d409aa26f00e180a5e0f9
sukumar <sukumarg.7310@gmail.com>
2025-07-11 09:33:51 +0530

    delete old logs

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,11 +7,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ ! -d SOURCE_DIR ]
-then
-    echo -e "$R Source directory: $SOURCE_DIR does not exist. $N"
-    exit 1
-fi 
+#if [ ! -d SOURCE_DIR ]
+#then
+    #echo -e "$R Source directory: $SOURCE_DIR does not exist. $N"
+    #exit 1
+#fi 
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +31 -name "*.log")
 
